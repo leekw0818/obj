3e7911fbd88eb092f0be0c1f4d3269b6480e80ea
Jerry Lee <leekw0818@naver.com>
2016-05-24 11:02:08 +0900

    Initial Commit

---
+++ ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC
+//
+//  Created by MF839-002 on 2016. 5. 24..
+//  Copyright © 2016년 Jerry CO.LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
